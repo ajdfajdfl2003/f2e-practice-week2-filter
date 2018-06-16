24a2929e3f5e3ff0d4fa41fca759a942b0e50703
AngusWu <anguswu@simbo.com.tw>
2018-06-16 20:54:19 +0800

    extract navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <div id="app">
-    <div class="filter-nav">
-      <b-container>
-        <b-row>
-          <b-col cols="4">
-            <div class="filter-nav-brand">
-              <a href="#">HaveFun</a>
-            </div>
-          </b-col>
-          <b-col cols="8">
-            <div class="filter-search-box">
-              <i class="fas fa-search"></i>
-              <b-form-input id="filter-search" type="text" placeholder="Explore your own activities"></b-form-input>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+    <FilterNavbar />
   </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import FilterNavbar from './components/Navbar'
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    FilterNavbar
+  }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="filter-nav">
+    <b-container>
+      <b-row>
+        <b-col cols="4">
+          <div class="filter-nav-brand">
+            <a href="#">HaveFun</a>
+          </div>
+        </b-col>
+        <b-col cols="8">
+          <div class="filter-search-box">
+            <i class="fas fa-search"></i>
+            <b-form-input id="filter-search" type="text" placeholder="Explore your own activities"></b-form-input>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
