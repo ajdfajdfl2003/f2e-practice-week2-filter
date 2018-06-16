3c81e84082ee9536370936730f3c7b0b9b926ebe
AngusWu <anguswu@simbo.com.tw>
2018-06-16 20:21:28 +0800

    finish nav bar style

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="app">
+    <div class="filter-nav">
+      <b-container>
+        <b-row>
+          <b-col cols="4">
+            <div class="filter-nav-brand">
+              <a href="#">HaveFun</a>
+            </div>
+          </b-col>
+          <b-col cols="8">
+            <div class="filter-search-box">
+              <i class="fas fa-search"></i>
+              <b-form-input id="filter-search" type="text" placeholder="Explore your own activities"></b-form-input>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'App',
-  components: {
-
-  }
+  components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import './assets/all.scss'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
