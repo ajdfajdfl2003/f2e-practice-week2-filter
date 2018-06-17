aff4e0ea4ee7cfef164e2c0a47de0989a9fd52fd
AngusWu <anguswu@simbo.com.tw>
2018-06-17 14:41:55 +0800

    get record from openData only show 5 record

--- src/components/FilterMain.vue
+++ src/components/FilterMain.vue
@@ -21,49 +21,24 @@
          results by…
       </div>
       <ul>
-        <li class="item">
-          <img src="https://picsum.photos/600/300/?image=25"/>
+        <li class="item"
+            v-for="record in records"
+            :key="record.id">
+          <img v-bind:src="record.Picture1"/>
           <div class="detail">
-            <h4>Kogi Cosby sweater.</h4>
+            <h4>{{record.Name}}</h4>
             <p class="card-text">
-              Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscipit
-              est, tincidunt mattis lorem luctus id. Donec eget massa a diam condimentum pretium. Aliquam erat volutpat.
-              Integer ut tincidunt orci. Etiam tristique, elit ut consectetur iaculis, metus lectus mattis justo, vel
-              mollis eros neque quis augue. Sed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut
-              suscipit justo.
+              {{record.Description}}
             </p>
             <div class="section">
-              <div class="ticket-info">Ethan Foster</div>
-              <div class="tel">Entertainment</div>
+              <div class="ticket-info">{{record.Ticketinfo}}</div>
+              <div class="tel">{{record.Tel}}</div>
             </div>
             <div class="section">
               <i class="fas fa-map-marker-alt"></i>
-              <div class="address">Kaohsiung City</div>
+              <div class="address">{{record.Add}}</div>
               <i class="far fa-calendar-alt"></i>
-              <div class="open-time">2018/5/24 - 2018/5/31</div>
-            </div>
-          </div>
-        </li>
-        <li class="item">
-          <img src="https://picsum.photos/600/300/?image=25"/>
-          <div class="detail">
-            <h4>Kogi Cosby sweater.</h4>
-            <p class="card-text">
-              Donec euismod scelerisque ligula. Maecenas eu varius risus, eu aliquet arcu. Curabitur fermentum suscipit
-              est, tincidunt mattis lorem luctus id. Donec eget massa a diam condimentum pretium. Aliquam erat volutpat.
-              Integer ut tincidunt orci. Etiam tristique, elit ut consectetur iaculis, metus lectus mattis justo, vel
-              mollis eros neque quis augue. Sed lobortis ultrices lacus, a placerat metus rutrum sit amet. Aenean ut
-              suscipit justo.
-            </p>
-            <div class="section">
-              <div class="ticket-info">Ethan Foster</div>
-              <div class="tel">Entertainment</div>
-            </div>
-            <div class="section">
-              <i class="fas fa-map-marker-alt"></i>
-              <div class="address">Kaohsiung City</div>
-              <i class="far fa-calendar-alt"></i>
-              <div class="open-time">2018/5/24 - 2018/5/31</div>
+              <div class="open-time">{{record.Opentime}}</div>
             </div>
           </div>
         </li>
@@ -79,7 +54,8 @@ export default {
   name: 'FilterMain',
   data () {
     return {
-      totalRecord: ''
+      totalRecord: '',
+      records: []
     }
   },
   methods: {
@@ -88,13 +64,14 @@ export default {
       if (resp.data.success) {
         const result = resp.data.result
         vm.totalRecord = result.total
-        console.log(result)
+        vm.records = result.records
+        console.log(vm.records)
       }
     }
   },
   created () {
     const vm = this
-    axios.get('https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97')
+    axios.get('https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97&limit=5')
       .then(resp => vm.getData({resp}))
       .catch(error => console.error(error))
   }
